33f9dcd5a3dd3b36e80b7634f305ec86c91047bf
dab105 <syxtracode@gmail.com>
2021-09-23 20:39:50 +0200

    added file

---
+++ main.js
@@ -0,0 +1,13 @@
+//  let, const 
+
+// String, Numbers, Boolean, null, undefined, Symbol
+
+const name = 'John';
+const  age = 30;
+
+// Concatenation
+console.log('My name is'+name+'and I am '+ age)
+
+
+//Template String
+console.log(`My name is ${name} and I am ${age} old.`)
